a0757fe8f34a55fba2bde0e3113bc61caa17ed7b
INEZARENE Hamed Abdelaziz <95131724+Inzhamed@users.noreply.github.com>
2025-02-10 16:40:18 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# function that adds two numbers
+def add(a, b):
+    return a + b
+
+# function that subtracts two numbers
+def subtract(a, b):
+return a - b
+
+# call the add function
+print(add(1, 2))
